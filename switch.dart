dc0f8efc878bba889089698cb49d0c78ece7a472
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-04-29 22:29:34 +0300

    switch statement

---
+++ switch.dart
@@ -0,0 +1,17 @@
+void main() {
+  var age = 18;
+
+  switch(age) {
+    case 19:
+      print("Old enough");
+      break;
+    case 20:
+      print("Still good");
+      break;
+    case 89:
+      print("Too old");
+      break;
+    default:
+      print("Whats your age");
+  }
+}
