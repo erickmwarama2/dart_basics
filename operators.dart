a4c87f5d9991e39ac12ebd1999d0d6fd52993e77
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-04-29 22:18:53 +0300

    learn dart operators

---
+++ operators.dart
@@ -0,0 +1,13 @@
+void main() {
+  int number = 34;
+  int numberTwo = 2;
+
+  print(number);
+  print(numberTwo);
+
+  if (!(number != 100) || number <= 100) {
+    print('Number <= 100');
+  } else {
+    print('Number is != 100');
+  }
+}
